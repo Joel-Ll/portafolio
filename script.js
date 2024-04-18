f9a36ae02739d3f5c0ca29bcb6a7caacc7196d2b
Joel-Ll <joellanos654@gmail.com>
2024-04-18 13:59:04 -0400

    Ultimos Cambios

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("reactjs");
-        habilidades[3].classList.add("comunicacion");
-        habilidades[4].classList.add("trabajo");
-        habilidades[5].classList.add("creatividad");
+        habilidades[0].classList.add("htmlcss");
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("typescript");
+        habilidades[3].classList.add("reactjs");
+        habilidades[4].classList.add("nestjs");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
         // habilidades[8].classList.add("dedicacion");
         // habilidades[9].classList.add("proyect");
     }
